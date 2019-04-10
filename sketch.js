c5957a65b710501f096448a4bd796bfb84ec8921
you <you@test.org>
2019-04-10 15:59:55 +0300

    js update + styling

--- sketch.js
+++ sketch.js
@@ -1,9 +1,21 @@
+var count = 0;
+
 function setup() {
+	frameRate(3);
 	noCanvas();
 	loadStrings('owords.txt', gotOrientir);
 	loadStrings('rwords.txt', gotRedo);
 }
 
+function draw() {
+	background(220);
+	count = count+1;
+	console.log(count);
+	if (count > 80) {
+		count = 0;
+	}
+}
+  
 function gotOrientir(data1) {
 	text1 = data1;
 }
@@ -18,26 +30,16 @@ let myVar2 = setInterval(changeRedo, 500);
 function changeOrientir() {
 		let index1 = round(random(text1.length));
 		var orientir = document.getElementById('orientir');
-		orientir.innerHTML = text1[index1];
+		orientir.innerHTML = text1[count];
 		console.log(index1);
 }
 
 function changeRedo() {
 	let index2 = round(random(text2.length));
 	var orientir = document.getElementById('redo');
-	orientir.innerHTML = text2[index2];
+	orientir.innerHTML = text2[count];
 	console.log(index2);
 }
 
-function sayHi(){
-	var r = Math.floor(Math.random() * 256);
-	var g = Math.floor(Math.random() * 256);
-	var b = Math.floor(Math.random() * 256);
-  
-	document.body.style.backgroundColor = "rgb("+r+","+g+","+b+")";
-  }
 
 
-function draw() {
-  background(220);
-}
